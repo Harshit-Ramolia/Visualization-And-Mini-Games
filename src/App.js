9ca9a786e17e21e36c433865599eb357945efdbf
Harshit-Ramolia <ramolia.mh@iitgn.ac.in>
2021-07-18 19:22:56 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { CircularProgress } from "@material-ui/core";
 import React, { Suspense } from "react";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Switch,
   Route,
   Redirect,
